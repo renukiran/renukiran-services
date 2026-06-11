a7cd05896ed2918fb97c271da4496e681872a19e
Ashok <ashok.kumar.kopparapudevagiri@gmail.com>
2026-06-11 07:15:17 +0530

    Issue fixes

--- src/main/java/com/renukiran/RenukiranServicesApplication.java
+++ src/main/java/com/renukiran/RenukiranServicesApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RenukiranServicesApplication {
 
 	public static void main(String[] args) {
+		java.util.TimeZone.setDefault(
+				java.util.TimeZone.getTimeZone("UTC")
+		);
 		SpringApplication.run(RenukiranServicesApplication.class, args);
 	}
 
